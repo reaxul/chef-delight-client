7e06ecca861e227265c126884d66a3f49ea578f0
DevReaxul <hreazul11@gmail.com>
2023-05-05 12:42:48 +0600

    img height fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import BannerSection from '../components/BannerSection';
+import React from "react";
+import BannerSection from "../components/BannerSection";
+import { useLoaderData } from "react-router-dom";
+import { Button, Card, Col, Row } from "react-bootstrap";
 
 const Home = () => {
-    return (
-        <div>
-            <BannerSection></BannerSection>
-        </div>
-    );
+  const chefs = useLoaderData();
+  return (
+    <div>
+      <BannerSection></BannerSection>
+      <Row className="justify-content-center container mx-auto">
+        {chefs.map((chef, index) => (
+          <Col md={4} key={index}>
+            <Card className="mb-4">
+              <Card.Img  style={{ height: "200px", objectFit: "cover" }} variant="top" src={chef.image} />
+              <Card.Body>
+                <Card.Title>{chef.name}</Card.Title>
+                <Card.Text>
+                  {chef.experience} years of experience <br />
+                  {chef.recipes} recipes <br />
+                  {chef.likes} likes
+                </Card.Text>
+                <Button variant="primary">View Recipes</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -11,7 +11,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element:<Home></Home>
+                element: <Home></Home>,
+                loader:()=>fetch('http://localhost:3000/chef')
             },
             
         ]
