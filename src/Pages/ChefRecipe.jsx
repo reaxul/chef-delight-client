96189e0f0762d0f01db0b34ee217c0d255fda870
DevReaxul <hreazul11@gmail.com>
2023-05-05 17:51:45 +0600

    chefs banner done

--- src/Pages/ChefRecipe.jsx
+++ src/Pages/ChefRecipe.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import RecipeCardGroup from "../components/RecipeCardGroup";
 
 const ChefRecipe = () => {
-    const { id } = useParams();
-    const chefs = useLoaderData();
-    return (
-        <div>
-            <h3>chef's recipe coming soon</h3>
-        </div>
-    );
+  const { id } = useParams();
+    const chef = useLoaderData();
+  return (
+    <div>
+      <Container fluid className="bg-dark text-white py-5">
+      <Row className="justify-content-center align-items-center">
+        <Col md={4} className="text-center">
+          <Image src={chef.image} roundedCircle fluid />
+          <h1 className="my-3">{chef.name}</h1>
+          <p>{chef.bio}</p>
+          <p>{chef.likes} Likes</p>
+          <p>{chef.recipes.length} Recipes</p>
+          <p>{chef.experience} Years of Experience</p>
+        </Col>
+      </Row>
+          </Container>
+          <RecipeCardGroup recipes={chef}></RecipeCardGroup>
+    </div>
+  );
 };
 
 export default ChefRecipe;
---
+++ src/components/RecipeCardGroup.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+const RecipeCardGroup = ({ recipes }) => {
+  return (
+    <div className="d-flex flex-wrap justify-content-center">
+      {recipes.map((recipe, index) => (
+        <Card key={index} className="m-3" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={recipe.image} />
+          <Card.Body>
+            <Card.Title>{recipe.name}</Card.Title>
+            <Card.Text>
+              <h6>Ingredients:</h6>
+              <ul>
+                {recipe.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+              <h6>Cooking Method:</h6>
+              <p>{recipe.method}</p>
+              <h6>Rating:</h6>
+              <p>{recipe.rating}</p>
+            </Card.Text>
+            <Button variant="primary">Favorite</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeCardGroup;
